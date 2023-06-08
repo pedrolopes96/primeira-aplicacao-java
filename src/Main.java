62089c632194c2dcf13447f807e1d595f7a3c359
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-06-08 19:19:19 -0300

    catálogo de filmes

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: Top Gun - Maverick ");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int anoDeLancamento = 2022;
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 8.1;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        //Média calculada a partir da opinião de três pessoas
+        double media = (9.8 + 6.3 + 8.0) / 3;
+        System.out.println(media);
+        String sinopse;
+        sinopse = """
+                Filme Top Gun 
+                Filme de aventura com ator dos anos 80
+                Muito bom!
+                A média do filme é: %f
+                Ano de lançamento %d
+                """ .formatted(media, anoDeLancamento);
+        System.out.println(sinopse);
     }
 }
