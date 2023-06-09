f275fe80154631e273eeedc868633c804060fc18
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-06-09 08:06:15 -0300

    loop

---
+++ src/Loop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Loop {
+    public static void main(String[] args) {
+        Scanner leitura = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+
+        }
+
+        System.out.println("Qual o ano de lançamento? ");
+        int anoDeLancamento = leitura.nextInt();
+    }
+}
