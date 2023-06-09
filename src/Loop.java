bd81293e69c98ff050d16fd806baae82af526ff7
Pedro Fernandes Lopes <pedroflopes1710@gmail.com>
2023-06-09 08:24:20 -0300

    loop

--- src/Loop.java
+++ src/Loop.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 public class Loop {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        double mediaAvaliacao = 0;
+        double nota = 0;
 
         for (int i = 0; i < 3; i++) {
+            System.out.println("Informe a sua nota para o filme: ");
+            nota = leitura.nextDouble();
+            mediaAvaliacao += nota;
 
         }
 
-        System.out.println("Qual o ano de lançamento? ");
-        int anoDeLancamento = leitura.nextInt();
+        System.out.println("Média de avalações" + mediaAvaliacao/3);
+
+
     }
 }
